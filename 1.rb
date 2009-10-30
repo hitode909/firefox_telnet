6e653d197bf2d453c9f10a217d724334223acdd4
hitode909 <hitode909@gmail.com>
2009-10-30 22:03:03 +0900

    it works

--- 1.rb
+++ 1.rb
@@ -4,14 +4,29 @@ require 'timeout'
 require 'socket'
 require 'json'
 
-module Connection
-  def self.do(str)
-    agent.puts(str)
-    logger.debug("send: #{str}")
-    read
+class Connection
+  def initialize(host = 'localhost', port = 4242)
+    @host = host
+    @port = port
+    logger.debug("instance created")
   end
 
-  def self.read
+  def do(str)
+    puts(str)
+    read
+  rescue Errno::EPIPE, Errno::ECONNRESET => e
+    logger.error("connection seems broken")
+    @agent = nil
+    read
+    retry
+  end
+
+  def puts(str)
+    agent.puts(str)
+    logger.debug("send: #{str}")
+  end
+
+  def read
     result = ""
     begin
       timeout(0.1) do
@@ -21,23 +36,26 @@ module Connection
       end
     rescue Timeout::Error
     end
+    logger.debug("receive: #{result}")
     result
   end
 
-  def self.agent
+  def agent
     unless @agent
-      logger.debug("connect")
-      @agent = TCPSocket.new("localhost", 4242)
+      logger.info("connect to #{@host}:#{@port}")
+      @agent = TCPSocket.new(@host, @port)
     end
     @agent
   end
 
-  def self.logger
+  def logger
     @logger ||= Logger.new(STDOUT)
   end
 end
 
-puts Connection.do <<EOF
+c = Connection.new
+c.logger.level = Logger::FATAL
+c.do <<EOF
 if (recordevents) {
   document.removeEventListener("click", recordevents, true);
 }
@@ -55,18 +73,17 @@ var recordevents = function(e) {
   o.target = t;
   events.push(uneval(o));
   };
-events = [];
+if (!events) events = [];
 document.addEventListener("click", recordevents, true);
 EOF
 
 begin
   loop do
-    event = Connection.do "events.pop()"
+    event = c.do "events.pop()"
     if event.size > 0
-        puts event
+      puts event.gsub(/\\u(\w{4})/){ |s| [$1.to_i(16)].pack('U')}
     end
-    sleep 1
   end
 rescue Interrupt
-  Connection.do "exit"
+  c.do "exit"
 end
